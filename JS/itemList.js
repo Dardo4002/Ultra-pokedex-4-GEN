0038c2fc6361cd5ef327eccf34afc3f429178264
Weather2Report <alvarobenedisan@gmail.com>
2025-05-19 21:41:21 +0200

    Loading

--- JS/itemList.js
+++ JS/itemList.js
@@ -88,7 +88,8 @@ $(document).ready(function(){
             $(".select-btn").on("click", function() {
                 
                 let itemId = $(this).attr("id");
-                console.log(itemId);
+                
+                loading();
                 select(itemId);
             });
 
@@ -97,13 +98,16 @@ $(document).ready(function(){
         function select(itemId){
             
             fetch("https://pokeapi.co/api/v2/item/" + itemId)
+
         .then(function(response){
             return response.json();
+            
             
         }
             
         )
         .then(function(result){
+            
             console.log(result);
             let selectedItem = result;
             $("#selected").empty();
@@ -151,5 +155,47 @@ $(document).ready(function(){
 
             
             
+        }
+        function loading(){
+            console.log("loading");
+            
+            $("#selected").empty();
+            let itemInfo = `<div class="col-12">
+                            <div class="row align-items-center">
+                                <!-- Imagen a la izquierda, ocupa el 50% -->
+                                <div class="col-6  d-flex justify-content-center">
+                                    <div class="spinner-border" role="status">
+                                         
+                                     </div>
+                                </div>
+
+                                <!-- Contenido textual a la derecha -->
+                                <div class="col-6 d-flex flex-column justify-content-center part-right-cont">
+                                    <!-- Nombre ocupa la mitad derecha (50% de col-6 = col-12 dentro del contenedor) -->
+                                    <div class="w-100 h-50 d-flex justify-content-center align-items-center text-center mb-2 pantalla-arriba mt-5 rounded-4 borde">
+                                        <p class="mb-0">Name: <br> Loading...</p>
+                                    </div>
+
+                                    <!-- Tipo y Precio juntos en fila -->
+                                    <div class="d-flex align-items-center justify-content-between gap-3 h-50 mt-2">
+                                        <div class="w-50 h-100 d-flex justify-content-center align-items-center text-center pantalla-arriba rounded-4 borde">
+                                         <p class="mb-0">Category: <br> Loading...</p>       
+                                        </div>
+                                        <div class="w-50 h-100 d-flex justify-content-center align-items-center text-center pantalla-arriba rounded-4 borde">
+                                            <p class="mb-0">Price: <br> Loading... Pokecoins</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Mitad de abajo -->
+                        <div class="col-12 part-down-cont mt-3 rounded-4 d-flex justify-content-center align-items-center borde">
+                            <p class="mb-0 text-center">Loading...</p>
+                        </div>`;
+
+            $("#selected").append(itemInfo);
+
         }
 })
